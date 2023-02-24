db1099d6901503ecef143a535125f998c827fdfd
IanniMuliterno <iannimuliterno@gmail.com>
2023-02-24 17:59:36 -0300

    Commit 2 - dois primeiros graficos inseridos

--- teste.R
+++ teste.R
@@ -1,3 +1,10 @@
+# proximos passos
+
+#concluir codigo
+#melhorar readme
+#falar com a Bea para ver como dou um push pro repositorio do blog
+##perguntar se rola um tutorial de quarto
+
 library(tidyverse)
 library(readr)
 library(ggplot2)
@@ -31,8 +38,20 @@ paralympic_swimming <- paralympic_byMedal %>%
   left_join(paralympic_total, by = "abb") %>%
   rename(number = n.x, total_medals = n.y)
 
-plot2 <- paralympic_swimming %>%
- # filter(abb %in% c("GBR", "USA", "AUS", "ESP", "CAN")) %>%
+#garantindo que no plot de top five teremos ordenação de acordo com o país que ganhou mais 
+top5 <- paralympic_total$abb[1:5]
+tail5 <- paralympic_total$abb[(nrow(paralympic_total)-4):nrow(paralympic_total)] 
+
+table_top5 <- paralympic_swimming %>% 
+  filter(abb %in% top5) %>%
+  mutate(abb = factor(abb,levels = top5)) 
+
+table_tail5 <- paralympic_swimming %>% 
+  filter(abb %in% tail5) %>%
+  mutate(abb = factor(abb,levels = tail5)) 
+
+
+plot2 <- table_top5 %>% 
   ggplot(aes(x = abb, y=number, fill = medal))+
   geom_col()+
   theme_minimal()+
@@ -46,10 +65,28 @@ plot2 <- paralympic_swimming %>%
         panel.grid.minor.x = element_blank(),
         panel.grid.major.y = element_blank(),
         panel.grid.minor.y = element_blank(), 
-        axis.text = element_text(family = "mono", face = "bold", size = 8))
+        axis.text = element_text(family = "mono", face = "bold", size = 8))+
+  ggtitle("Medalhas de ouro, prata e bronze conquistada pelos países de melhor desempenho")
 
 
-#Criando um gráfico de dispersão para mostrar a relação entre o tempo de natação e a idade dos atletas:
+plot3 <- table_tail5 %>% 
+  ggplot(aes(x = abb, y=number, fill = medal))+
+  geom_col()+
+  theme_minimal()+
+  scale_fill_manual(values = c("#D89581", "#D4AF37", "#C0C0C0"))+
+  scale_x_discrete(expand = c(0,0)) +
+  labs(x="", y="", fill = "", title = "Swimming medals for top 5 countries")+
+  theme(legend.position = "none",
+        plot.title.position = "plot", 
+        plot.title = element_text(family = "mono", face = "bold", size = 8),
+        panel.grid.major.x = element_blank(), 
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank(), 
+        axis.text = element_text(family = "mono", face = "bold", size = 8))+
+  ggtitle("Medalhas de ouro, prata e bronze conquistada pelos países de pior desempenho")
+
+plot3
 # gráfico de dispersão é valido !
 # juntando as bases, vai ficar menor a amostra, mas vale mostrar a relação entre idade e numero de medalhas
 
